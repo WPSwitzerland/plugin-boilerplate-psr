2b650a82e463eacf156c11eb505769fa63f7a4c6
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-02-25 12:42:48 +0100

    Update wp-plugin-default.php
    Add markers for automated search-and-replace.

--- wp-plugin-default.php
+++ wp-plugin-default.php
@@ -1,17 +1,17 @@
 <?php
 /*
-Plugin Name: PLUGIN NAME
-Plugin URI: PLUGIN URI
-Description: DESCRIPTION
-Author: PLUGIN AUTHOR
+Plugin Name: ###PLUGIN_NAME###
+Plugin URI: ###PLUGIN_URI###
+Description: ###PLUGIN_DESCRIPTION###
+Author: ###PLUGIN_AUTHOR###
 Version: 0.0.1
-Author URI: AUTHOR URI
-Text Domain: TEXT-DOMAIN
-Domain Path: /Resources/Private/Language
+Author URI: ###AUTHOR_URI###
+Text Domain: ###TEXT_DOMAIN###
+Domain Path: /languages
 */
 
 if (version_compare($wp_version, '4.6', '<') || version_compare(PHP_VERSION, '5.3', '<')) {
-    function PLUGIN_PREFIX_compatability_warning()
+    function ###PLUGIN_PREFIX###_compatability_warning()
     {
         echo '<div class="error"><p>'.sprintf(
             __('“%1$s” requires PHP %2$s (or newer) and WordPress %3$s (or newer) to function properly. Your site is using PHP %4$s and WordPress %5$s. Please upgrade. The plugin has been automatically deactivated.', 'TEXT-DOMAIN'),
@@ -25,13 +25,13 @@ if (version_compare($wp_version, '4.6', '<') || version_compare(PHP_VERSION, '5.
             unset($_GET['activate']);
         }
     }
-    add_action('admin_notices', 'PLUGIN_PREFIX_compatability_warning');
+    add_action('admin_notices', '###PLUGIN_PREFIX###_compatability_warning');
 
-    function PLUGIN_PREFIX_deactivate_self()
+    function ###PLUGIN_PREFIX###_deactivate_self()
     {
         deactivate_plugins(plugin_basename(__FILE__));
     }
-    add_action('admin_init', 'PLUGIN_PREFIX_deactivate_self');
+    add_action('admin_init', '###PLUGIN_PREFIX###_deactivate_self');
 
     return;
 } else {
