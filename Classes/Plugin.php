c903c97c36bcbe3666af5a49279cd3e1d28a7d68
Nico Martin <mail@nicomartin.ch>
2017-04-17 09:19:57 +0200

    changed it to an object based structure

--- Classes/Plugin.php
+++ Classes/Plugin.php
@@ -4,6 +4,12 @@ namespace AUTHOR_NAMESPACE\PLUGIN_NAMESPACE;
 
 class Plugin
 {
+
+    public $name    = '';
+    public $pfx     = '';
+    public $version = '';
+    public $file    = '';
+
     public function dump($var, $die = false)
     {
         echo '<pre>' . print_r($var, 1) . '</pre>';
@@ -14,6 +20,22 @@ class Plugin
 
     public function __construct()
     {
+        $this->name = 'PLUGIN_NAME';
+    }
+
+    public function set_PluginData($file)
+    {
+
+        $data = get_plugin_data($file);
+
+        $this->name     = $data['Name'];
+        $this->pfx      = 'PLUGIN_PREFIX';
+        $this->version  = $data['Version'];
+        $this->file     = $file;
+    }
+
+    public function run()
+    {
         add_action('plugins_loaded', array($this, 'loadPluginTextdomain'));
     }
 
@@ -22,8 +44,6 @@ class Plugin
      */
     public function loadPluginTextdomain()
     {
-        load_plugin_textdomain('TEXT_DOMAIN', false, dirname(dirname(plugin_basename(__FILE__))) . '/languages');
+        load_plugin_textdomain('TEXT_DOMAIN', false, dirname(plugin_basename($this->file)) . '/languages');
     }
 }
-
-new Plugin();
