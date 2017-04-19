dea6b0a936ca95108a32de7cea13e1404115cfb7
Nico Martin <mail@nicomartin.ch>
2017-04-19 17:41:07 +0200

    changed PLUGIN_KEY() to AUTHOR_NAMESPACE_PLUGIN_KEY()

--- wp-plugin-default.php
+++ wp-plugin-default.php
@@ -38,9 +38,9 @@ if (version_compare($wp_version, '4.7', '<') || version_compare(PHP_VERSION, '5.
 
     require_once 'Classes/Plugin.php';
 
-    function PLUGIN_KEY()
+    function AUTHOR_NAMESPACE_PLUGIN_KEY()
     {
         return AUTHOR_NAMESPACE\PLUGIN_NAMESPACE\Plugin::getInstance(__FILE__);
     }
-    PLUGIN_KEY();
+    AUTHOR_NAMESPACE_PLUGIN_KEY();
 }
