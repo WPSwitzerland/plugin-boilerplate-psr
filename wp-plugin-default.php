95fc32dedca23b04b2f46f3a43000f9450d93499
Mark Howells-Mead <permanent.tourist@gmail.com>
2017-04-20 09:46:39 +0200

    Update wp-plugin-default.php
    Rename instance function to comply with non-namespace standard in this file.

--- wp-plugin-default.php
+++ wp-plugin-default.php
@@ -38,9 +38,9 @@ if (version_compare($wp_version, '4.7', '<') || version_compare(PHP_VERSION, '5.
 
     require_once 'Classes/Plugin.php';
 
-    function AUTHOR_NAMESPACE_PLUGIN_KEY()
+    function PLUGIN_PREFIX_get_instance()
     {
         return AUTHOR_NAMESPACE\PLUGIN_NAMESPACE\Plugin::getInstance(__FILE__);
     }
-    AUTHOR_NAMESPACE_PLUGIN_KEY();
+    PLUGIN_PREFIX_get_instance();
 }
