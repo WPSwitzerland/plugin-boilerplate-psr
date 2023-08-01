5d24ad5ff72322bd713a99fea690ba56cd0a69c9
Mark Howells-Mead <permanent.tourist@gmail.com>
2023-08-01 10:06:08 +0200

    Revise class instantiation

--- src/Plugin.php
+++ src/Plugin.php
@@ -5,10 +5,15 @@ namespace AUTHOR_NAMESPACE\PLUGIN_NAMESPACE;
 class Plugin
 {
 	private static $instance;
+	public $debug = false;
+	public $dir = '';
+	public $dirname = '';
+	public $file = '';
+	public $key = '';
 	public $name = '';
 	public $prefix = '';
+	public $url = '';
 	public $version = '';
-	public $file = '';
 
 	/**
 	 * Loads and initializes the provided classes.
@@ -50,16 +55,17 @@ class Plugin
 		if (!isset(self::$instance) && !(self::$instance instanceof Plugin)) {
 			self::$instance = new Plugin;
 
-			if (!function_exists('get_plugin_data')) {
-				include_once(ABSPATH . 'wp-admin/includes/plugin.php');
-			}
-
-			$data = get_plugin_data($file);
-
-			self::$instance->name = $data['Name'];
-			self::$instance->prefix = 'PLUGIN_PREFIX';
-			self::$instance->version = $data['Version'];
+			self::$instance->debug = defined('WP_DEBUG') && WP_DEBUG;
+			self::$instance->dir = plugin_dir_path(self::$instance->file);
+			self::$instance->dirname = dirname(plugin_basename(self::$instance->file));
 			self::$instance->file = $file;
+			self::$instance->prefix = 'PLUGIN_PREFIX';
+			self::$instance->url = plugin_dir_url(self::$instance->file);
+
+			$version = get_file_data($file, ['Version']);
+			$text_domain = get_file_data($file, ['Text Domain']);
+			self::$instance->version = $version[0] ?? '';
+			self::$instance->key = $text_domain[0] ?? '';
 
 			self::$instance->run();
 		}
